6be1de2b6b70136d18023a4b8384e43a40c16012
zhaodongyu <dongyu.zhao@accenture.com>
2020-08-28 10:33:17 +0800

    blog first commit test

---
+++ app.js
@@ -0,0 +1,39 @@
+// 引用模块
+var express = require('express');
+var mongoose = require('mongoose');   
+var bodyParser = require('body-parser')
+var path = require('path');
+var ejs = require('ejs');
+var app = express();
+
+var port = process.env.PORT || 3000;
+var router = require('./routes/index');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false})); 
+
+app.use('/', router);
+
+mongoose.connect('mongodb://localhost:27017/blog')     //连接本地数据库blog 
+
+var db = mongoose.connection;
+
+// 连接成功
+db.on('open', function(){
+    console.log('MongoDB Connection Successed');
+});
+
+// 连接失败
+db.on('error', function(){
+    console.log('MongoDB Connection Error');
+});
+
+app.set('views', path.join(__dirname,'views'));  // 设置视图文件目录
+
+app.set('view engine' , 'html'); //修改模板文件的后缀名为html
+app.engine('.html' , ejs.__express); 
+
+app.use( express.static(path.join(__dirname, 'public')) );  // 配置静态资源目录
+app.listen(port);  
+// 路由规则
+console.log('server started at port 3000');
